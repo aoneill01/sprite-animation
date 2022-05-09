b2252e71d76781b4b5d9416489d2c4811d7a61f3
Andy O'Neill <aoneill01@gmail.com>
2022-05-09 14:44:45 -0400

    Start of hex view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Animation from "./components/Animation";
 
 function App() {
+  const [sprites, setSprites] = useState();
+
+  useEffect(() => {
+    loadSprites();
+  }, []);
+
+  async function loadSprites() {
+    const response = await fetch("/5f");
+    const data = new Uint8Array(await response.arrayBuffer());
+    setSprites(data);
+  }
+
   return (
     <div className="App">
-      <Animation />
+      <Animation sprites={sprites} />
     </div>
   );
 }
--- src/components/Animation.js
+++ src/components/Animation.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { gsap } from "gsap";
 import "./Animation.css";
 
-function Animation() {
+function Animation({ sprites }) {
   useEffect(() => {
     const tl = gsap.timeline({ yoyo: true, repeat: -1 });
     tl.to({}, { duration: 0.5 });
@@ -148,10 +148,15 @@ function Animation() {
     );
 
     tl.to({}, { duration: 0.5 });
-  });
+
+    return () => {
+      tl.time(0);
+      tl.kill();
+    };
+  }, []);
 
   return (
-    <svg width="300" height="300">
+    <svg width="600" height="300">
       <g>
         <text
           x={blockSize * 4 + 1}
@@ -164,31 +169,32 @@ function Animation() {
         </text>
         <Byte id="binary" y="22" x="0" />
       </g>
+      <HexView y={blockSize * 6} data={sprites} />
     </svg>
   );
 }
 
 const blockSize = 22;
 
-function BlockValue({ x = 0, y = 0, id }) {
+function BlockValue({ x = 0, y = 0, id, value, width = blockSize }) {
   return (
     <g id={id} transform={`translate(${x}, ${y})`}>
       <rect
         x="0"
         y="0"
-        width={blockSize}
+        width={width}
         height={blockSize}
         stroke="0"
         fill="#ddd"
       />
       <text
-        x="11"
+        x={width / 2}
         y="13"
         fill="#333"
         textAnchor="middle"
         dominantBaseline="middle"
       >
-        0
+        {value ?? 0}
       </text>
     </g>
   );
@@ -204,17 +210,32 @@ function Nibble({ x = 0, y = 0, id }) {
 }
 
 function Byte({ x = 0, y = 0, id }) {
-  const children = [];
-  for (let i = 0; i < 4; i++) {
-    children.push(<BlockValue x={i * blockSize} id={`${id}-${i}`} key={i} />);
-  }
-
   return (
     <g transform={`translate(${x}, ${y})`}>
       <Nibble id={`${id}-${0}`} />
       <Nibble id={`${id}-${1}`} x={blockSize * 4 + 2} />
     </g>
   );
+}
+
+function HexView({ x = 0, y = 0, data }) {
+  const children = [];
+  if (data) {
+    for (let addr = 0; addr < 16; addr++) {
+      let x = addr * 30;
+      if (addr >= 8) x += 2;
+      children.push(
+        <BlockValue
+          x={x}
+          value={data[addr].toString(16).padStart(2, "0")}
+          key={addr}
+          width={30}
+        />
+      );
+    }
+  }
+
+  return <g transform={`translate(${x}, ${y})`}>{children}</g>;
 }
 
 export default Animation;
